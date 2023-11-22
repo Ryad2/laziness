237da499ae8ebe34563a2fed657e6484624fcbb1
ryada <ryad.aouak@gmail.com>
2023-11-22 17:27:14 +0100

    last commit

--- src/main/scala/laziness/IntLazyLists.scala
+++ src/main/scala/laziness/IntLazyLists.scala
@@ -4,21 +4,29 @@ import MyLazyList.*
 
 object IntLazyLists:
   def from(x: Int): MyLazyList[Int] =
-    ???
+    cons(x, from(x + 1))
   def range(x: Int, y: Int): MyLazyList[Int] =
-    ???
+    from(x).take(y - x)
+
+
   val anonymList: MyLazyList[Int] = cons(1, anonymList)
-  lazy val infiniteTwoes: MyLazyList[Int] = ???
+  lazy val infiniteTwoes: MyLazyList[Int] = anonymList.map(_ => 2)
+
+
   val naturalNumbers1: MyLazyList[Int] = from(0)
-  lazy val naturalNumbers2: MyLazyList[Int] = cons(0, ???)
+
+  lazy val naturalNumbers2: MyLazyList[Int] =
+    cons(0, naturalNumbers2.map(_ + 1))
+
   lazy val primeNumbers: MyLazyList[Int] =
     import MyLazyListState.*
     def sieve(s: MyLazyList[Int]): MyLazyList[Int] = ???
     sieve(???)
-  lazy val fib: MyLazyList[Int] = cons(0, cons(1, ???))
-  def ack(m: Int, n: Int): Int =
-    if m == 0 then n + 1
-    else if n == 0 then ack(m - 1, 1)
-    else ack(m - 1, ack(m, n - 1))
-  lazy val ackermann: MyLazyList[MyLazyList[Int]] =
-    ???
+
+
+  lazy val fib: MyLazyList[Int] =
+    cons(0,
+      cons(1,
+        fib.zip(fib.tail).map ( (x, y) => x + y )
+      )
+    )
--- src/main/scala/laziness/OtherPractice.scala
+++ src/main/scala/laziness/OtherPractice.scala
@@ -1,16 +1,52 @@
 package laziness
 
 import MyLazyList.*
+import laziness.MyLazyListState.LZCons
 
 object OtherPractice:
   lazy val codes: MyLazyList[String] =
-    ???
+    cons( "0",
+      cons("1",
+        codes.flatMap(
+          s => cons("0" + s, cons("1" + s, empty))
+        )
+      )
+    )
+
   lazy val palCodes: MyLazyList[String] =
-    ???
+    codes.filter(s => s == s.reverse)
+
   val middle: MyLazyList[String] = cons("", cons("0", cons("1", empty)))
-  lazy val palCodes2: MyLazyList[String] =
-    // need to add base cases at the beginning
-    cons("0", cons("1", empty)).append(???)
+  lazy val palCodes2: MyLazyList[String] = codes.flatMap(c => middle.map(m => c + m + c.reverse))
+  // need to add base cases at the beginning
+    /*cons("0", cons("1", empty))
+      .append(
+        codes.flatMap(c => middle.map(m => c + m + c.reverse))
+      )*/
+
   def nextLine(currentLine: List[Int]): List[Int] =
-    ???
+    currentLine.foldLeft(List(1))(
+      (acc, i) =>
+
+
   lazy val funSeq: MyLazyList[List[Int]] = ???
+
+
+
+add $t0, $zero, $zero
+add $v0, $zero, $zero
+add $v1, $zero, $zero
+Loop:
+  sltu $t2, $t0, $a1
+beq $t2, $zero, fin
+lw $t1, 0($a0)
+sltu $t2, $t1, $v0
+bne $t2, $zero, skip
+add $v0, $t1, $zero
+add $v1, $t0, $zero
+Skip:
+  addi $t0, $t0, 1
+addi $a0, $a0, 4
+j loop
+  fin:
+
